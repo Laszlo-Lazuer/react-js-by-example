7186bf22f9c0ff9d21ea8c15fd8347fb723f4dd8
Laszlo-Lazuer <visualjazz@gmail.com>
2018-05-02 13:01:30 -0500

    Updates the logic to support changing the content manually.

--- hello-world/src/helloWorld/helloWorld.js
+++ hello-world/src/helloWorld/helloWorld.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 
 class HelloWorld extends Component {
-  linkClicked(event) {
-    console.log(event);
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+  }
+
+  linkClicked = (event) => {
+    this.setState({
+      clicked: true
+    });
   }
 
   render () {
+    if (this.state.clicked === false) {
     return (
       <div id="two-tags">
         <h1>Hello {this.props.name}!</h1>
         <a href="#" id="click" onClick={this.linkClicked}>Click Me</a>
       </div>
-    )
+    );
+  } else {
+    return (
+      <div id="two-tags">
+        <h1>Hello {this.props.name}</h1>
+        <span>You clicked the link</span>
+      </div>
+    );
+  }
   }
 }
 
