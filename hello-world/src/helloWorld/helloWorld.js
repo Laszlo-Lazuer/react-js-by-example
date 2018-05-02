63b79f4e30fe3c5ac321e781d76b9677e6890b69
Laszlo-Lazuer <visualjazz@gmail.com>
2018-05-02 12:42:54 -0500

    Event - Adds event handler.

--- hello-world/src/helloWorld/helloWorld.js
+++ hello-world/src/helloWorld/helloWorld.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class HelloWorld extends Component {
+  linkClicked(event) {
+    console.log(event);
+  }
+
   render () {
     return (
       <div id="two-tags">
         <h1>Hello {this.props.name}!</h1>
-        <a href="#" id="click">Click Me</a>
+        <a href="#" id="click" onClick={this.linkClicked}>Click Me</a>
       </div>
     )
   }
