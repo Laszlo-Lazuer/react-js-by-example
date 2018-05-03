5cf7c820ab89a01ca5b714c53f08234d263fcc58
Laszlo-Lazuer <visualjazz@gmail.com>
2018-05-03 10:55:14 -0500

    Completed.

--- country-prefix/src/components/prefixer.js
+++ country-prefix/src/components/prefixer.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react';
 
+let prefixes = {
+  "Germany": '+49',
+  "Poland": '+48',
+  "Serbia": '+381',
+};
+
 class Prefixer extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    let country = this.props.initialCountry || "Poland";
+    this.state = {
+      country: country
+    };
+  }
+
+  handleChange = (e) => {
+    let newCountry = e.target.value;
+    this.setState({
+      country: newCountry
+    });
+  }
+
+  countryItem(country, index) {
     return (
-      <div class="form-horizontal">
-        <div class="form-group">
-          <label for="selectbox" class="col-sm-3 control-label">Country</label>
-          <div class="col-sm-3">
-            <select class="form-control" id="selectbox"> <option value="Poland">Poland</option>
+      <option value={country} key={index}>{country}</option>
+    );
+  }
+
+  render() {
+    let prefix = prefixes[this.state.country];
+    return (
+      <div className="form-horizontal">
+        <div className="form-group">
+          <label for="selectbox" className="col-sm-3 control-label">Country</label>
+          <div className="col-sm-3">
+            <select className="form-control" id="selectbox" value={this.state.country} onChange={this.handleChange}>
+              {Object.keys(prefixes).map(this.countryItem)}
             </select>
           </div>
         </div>
-        <div class="form-group">
-          <label for="input-text" class="col-sm-3 control-label">Phone number</label>
-          <div class="col-sm-3">
-            <div class="input-group">
-              <span class="input-group-addon">+48</span>
-              <input type="text" class="form-control" id="input-text" />
+        <div className="form-group">
+          <label for="input-text" className="col-sm-3 control-label">Phone number</label>
+          <div className="col-sm-3">
+            <div className="input-group">
+              <span className="input-group-addon">{prefix}</span>
+              <input type="text" className="form-control" id="input-text" />
             </div>
           </div>
         </div>
