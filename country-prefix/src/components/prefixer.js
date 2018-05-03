714fbcc17798eec32d18567049e10f3a75380c75
Laszlo-Lazuer <visualjazz@gmail.com>
2018-05-03 10:33:05 -0500

    Updates styles.

---
+++ country-prefix/src/components/prefixer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Prefixer extends Component {
+  render() {
+    return (
+      <div class="form-horizontal">
+        <div class="form-group">
+          <label for="selectbox" class="col-sm-3 control-label">Country</label>
+          <div class="col-sm-3">
+            <select class="form-control" id="selectbox"> <option value="Poland">Poland</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="input-text" class="col-sm-3 control-label">Phone number</label>
+          <div class="col-sm-3">
+            <div class="input-group">
+              <span class="input-group-addon">+48</span>
+              <input type="text" class="form-control" id="input-text" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Prefixer;
